1d67f891e7f3b40b72a3b7d32a2afdd0f7bd72b3
Theron <tspiegl@gmail.com>
2017-11-12 14:10:42 -0500

    apple menu function fix, about formatting tweak.

--- guiMenu_darwin.go
+++ guiMenu_darwin.go
@@ -2,8 +2,8 @@ package main
 
 import "github.com/dontpanic92/wxGo/wx"
 
-func addAboutToOSXMenu() {
+func addAboutToOSXMenu(menuBar wx.MenuBar) {
 	menu := wx.NewMenu()
-	menu = wx.OSXGetAppleMenu()
+	menu = menuBar.OSXGetAppleMenu()
 	menu.Append(wx.ID_ABOUT)
 }
