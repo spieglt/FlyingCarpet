b99aa52fcf57883dbe3cbb8970c65c25d65ed9cb
Theron <tspiegl@gmail.com>
2019-01-03 18:33:36 -0600

    tweak build script

--- wg_rebuild.ps1
+++ wg_rebuild.ps1
@@ -1,17 +1,18 @@
 # clean up old executable
 rm "./Flying Carpet.exe"
 
-# copy file with icon
+# copy file with icon for real and wrapper programs
+# fc.syso is automatically detected for inclusion
+# by `go build` and `qtdeploy` during compilation
 Copy-Item .\icons\Windows\fc.syso .\gui\flyingcarpet\
 Copy-Item .\icons\Windows\fc.syso .\gui\
 
 # build with github.com/therecipe/qt
-cd .\gui\flyingcarpet
-qtdeploy.exe build desktop
-cd ..\..
+qtdeploy.exe build desktop .\gui\flyingcarpet
 
-# use Windows SDK utility to embed application manifest, 
-# which will force Flying Carpet to launch "as Administrator" if the user has UAC enabled
+# use Windows SDK utility to embed application manifest,
+# which will force Flying Carpet to launch "as Administrator"
+# if the user has UAC enabled
 mt.exe -manifest .\gui\flyingcarpet\flyingcarpet.exe.manifest -outputresource:.\gui\flyingcarpet\deploy\windows\flyingcarpet.exe;1
 
 # copy WiFi Direct DLL to Qt output directory
@@ -22,8 +23,11 @@ cd gui
 rice.exe embed-go
 # extra flags prevent console window from showing while wrapper extracts files to temp directory
 go build -ldflags -H=windowsgui -o "Flying Carpet.exe"
-
-# execute
 mv -Force "Flying Carpet.exe" ..
 cd ..
+
+# zip to /bin
+Compress-Archive -Force -Path '.\Flying Carpet.exe' -DestinationPath '.\bin\Flying Carpet (Windows).zip'
+
+# execute
 cmd /C "./Flying Carpet.exe"
