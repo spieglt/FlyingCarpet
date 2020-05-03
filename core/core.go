05b67a98ea1e64e34acef7ba6bd63324a6ba058a
Theron <tspiegl@gmail.com>
2020-05-02 22:13:07 -0500

    version

--- core/core.go
+++ core/core.go
@@ -248,8 +248,8 @@ func GeneratePassword() string {
 }
 
 const AboutMessage = `https://github.com/spieglt/flyingcarpet
-Version: 2.0
-Copyright (c) 2019, Theron Spiegl. All rights reserved.
+Version: 2.1
+Copyright (c) 2020, Theron Spiegl. All rights reserved.
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
 * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
