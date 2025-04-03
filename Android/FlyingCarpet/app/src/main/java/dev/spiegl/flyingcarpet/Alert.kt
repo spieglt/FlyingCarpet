5db551f1df26f8150c1e61f334465632e590aef8
Theron <tspiegl@gmail.com>
2025-04-02 23:20:07 -0500

    status bar, stay at sdk 34, android version 9.0.2

--- Android/FlyingCarpet/app/src/main/java/dev/spiegl/flyingcarpet/Alert.kt
+++ Android/FlyingCarpet/app/src/main/java/dev/spiegl/flyingcarpet/Alert.kt
@@ -33,7 +33,7 @@ class About : DialogFragment() {
 
 const val AboutMessage = """
     https://flyingcarpet.spiegl.dev
-    Version 9.0.0
+    Version 9.0.2
     theron@spiegl.dev
     Copyright 2025, Theron Spiegl, all rights reserved.
 
