f7d86f7b0fca7aafd22cbebaeb13e0124caf4b7a
Theron <tspiegl@gmail.com>
2025-04-05 00:04:44 -0500

    version bump

--- Android/FlyingCarpet/app/src/main/java/dev/spiegl/flyingcarpet/Alert.kt
+++ Android/FlyingCarpet/app/src/main/java/dev/spiegl/flyingcarpet/Alert.kt
@@ -33,7 +33,7 @@ class About : DialogFragment() {
 
 const val AboutMessage = """
     https://flyingcarpet.spiegl.dev
-    Version 9.0.2
+    Version 9.0.3
     theron@spiegl.dev
     Copyright 2025, Theron Spiegl, all rights reserved.
 
